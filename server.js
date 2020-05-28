771fb2b51a9f420245762a8731b4787ff54da6b5
Dhwani desai <dhwanidesai173@gmail.com>
2020-05-28 16:28:06 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+//serve static assets
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/bulid'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
